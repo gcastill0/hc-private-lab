2fa1c436889a395fb176e7c18f25ebc49568afdb
Gilberto Castillo <gilberto.castillo@rogers.com>
2019-07-16 11:21:13 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "azure_resource_name_baseline" {
-  value = azurerm_resource_group.test.name
+  value = "${azurerm_resource_group.test.name}"
 }
 
 output "azure_resource_name_location" {
-  value = azurerm_resource_group.test.location
+  value = "${azurerm_resource_group.test.location}"
 }
