c31ebb9fe24ebe18a81fb3ad04313c51b059a342
gcastill0 <gilberto@hashicorp.com>
2020-04-22 15:27:54 -0400

    moving to USEast2

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "postfix" {
 
 variable "location" {
   description = "(Required) The location where the resource group should be created. See this: https://azure.microsoft.com/en-us/global-infrastructure/locations/"
-  default     = "canadacentral"
+  default     = "useast2"
 }
 
 variable "gwsubname" {
