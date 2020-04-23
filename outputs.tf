7de13bf901672a489182c18eb6c78b1536696fc6
gcastill0 <gilberto@hashicorp.com>
2020-04-22 22:12:34 -0400

    Fixed output

--- outputs.tf
+++ outputs.tf
@@ -10,6 +10,10 @@ output "azurerm_network_security_group" {
   value = "${azurerm_network_security_group.test.name}"
 }
 
+output "azure_virtual_network_name" {
+  value = "${azurerm_virtual_network.test.name}"
+}
+
 output "azurerm_subnet_internal_id" {
   value = "${azurerm_subnet.test.id}"
 }
@@ -20,8 +24,4 @@ output "postfix" {
 
 output "azure_vm_ssh-rsa" {
   value = "${var.hcadmin_rsa}"
-}
-
-output "azure_virtual_network_name" {
-  value = "${var.azurerm_virtual_network.test.name}"
 }
